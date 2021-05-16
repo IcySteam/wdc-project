9acf98ad22d2a4c804bbb128b41523feeeaea24e
IcySteam <IcySteam@users.noreply.github.com>
2021-05-16 16:46:42 +0930

    Update Footer.vue

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,37 +1,48 @@
 <template>
-  <v-footer
-    dark
-    padless
-    class="mt-10"
-  >
-    <v-card
-      tile
-      class="brown darken-4 white--text text-center"
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="i in 10"
+        :key="i"
+        cols="12"
+      />
+    </v-row>
+    <v-footer
+      absolute
+      dark
+      padless
+      class="mt-10"
     >
-      <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
+      <v-card
+        tile
+        class="brown darken-4 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </v-card-text>
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>WWWarriors</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>WWWarriors</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-container>
+
 </template>
 
 <script>
