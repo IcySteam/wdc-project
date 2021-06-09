aec36a33dece8d1cc70fffa0c34785c3be8b2f29
minhaj231 <64825702+minhaj231@users.noreply.github.com>
2021-06-10 04:16:54 +0930

    Added query route for user checkin history

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -28,4 +28,26 @@ router.get('/GetUsersDigest', function(req, res) {
   });
 });
 
+router.get('/GetUserCheckInHistory', function(req, res) {
+  //Connect to the database
+  req.pool.getConnection(function(err,connection) {
+    if (err) {
+      console.log(err);     //for error details
+      res.sendStatus(500);
+      return;
+    }
+    //query
+    var query = "SELECT venue.venueID, venue.name, checkIn.time, checkIn.ID FROM venue INNER JOIN checkIn ON venue.venueID = checkIn.venue WHERE checkIn.user = ?;";
+    connection.query(query, function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); //send response
+    });
+  });
+});
+
 module.exports = router;
