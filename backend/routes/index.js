9c24e19261ced627f7e5c8b556b914a47ab66a4a
minhaj231 <64825702+minhaj231@users.noreply.github.com>
2021-06-10 01:18:20 +0930

    Added query route for /GetUsersDigest

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/GetUsersDigest', function(req, res) {
+  //Connect to the database
+  req.pool.getConnection(function(err,connection) {
+    if (err) {
+      console.log(err);     //for error details
+      res.sendStatus(500);
+      return;
+    }
+    //query
+    var query = "SELECT userID, CONCAT(firstName,' ',lastName) AS fullName, recentlyBeenToHotspot, email FROM user WHERE usermode = 'user';";
+    connection.query(query, function(err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); //send response
+    });
+  });
+});
+
 module.exports = router;
