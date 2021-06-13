e78e8b4547c1932cea0b863fd8628e4de3b48b27
minhaj231 <64825702+minhaj231@users.noreply.github.com>
2021-06-13 23:17:27 +0930

    Added components such as Add hotspot, manage and delete hotspot venues

--- frontend/src/components/AdminAdministrationComponent.vue
+++ frontend/src/components/AdminAdministrationComponent.vue
@@ -3,6 +3,13 @@
     <h1 class="pb-10">Administration</h1>
     <ConsistentMP id="currentHotspots">
       <template>
+        <v-btn
+          color="teal accent-3"
+          plain
+          @click="addHotspot"
+        >
+          Add Hotspot
+        </v-btn>
         <v-card>
           <v-card-title>
             Current Hotspots
@@ -22,7 +29,181 @@
             :search="currentHotspotsSearch"
             sort-by="lastCreated"
             sort-desc
-          />
+          >
+            <template v-slot:top>
+              <v-dialog
+                v-model="HAdialog"
+                max-width="500px"
+              >
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Hotspot Details</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="addHotspotDetails.venueID"
+                            label="Venue ID"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="addHotspotDetails.startTime"
+                            label="Start Time"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="addHotspotDetails.endTime"
+                            label="End Time"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="addHotspotDetails.affectedUsers"
+                            label="Affected Users"
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                      color="teal accent-3"
+                      text
+                      @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="teal accent-3"
+                      text
+                      @click="submitHotspot"
+                    >
+                      Submit
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog
+                v-model="HMdialog"
+                max-width="800px"
+              >
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Manage Hotspot Details</span>
+                    <v-spacer />
+                    <span>Venue ID: {{ selectedVenueID }}</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="manageHotspotDetails.startTime"
+                            label="Start Time"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="manageHotspotDetails.endTime"
+                            label="End Time"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="manageHotspotDetails.affectedUsers"
+                            label="Affected Users"
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                      color="teal accent-3"
+                      text
+                      @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="teal accent-3"
+                      text
+                      @click="hsave"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="DelDialog" max-width="500px">
+                <v-card>
+                  <v-card-title class="text-h5">Are you sure you want to remove this venue from hotspot?</v-card-title>
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn color="teal accent-3" text @click="close">Cancel</v-btn>
+                    <v-btn color="teal accent-3" text @click="deleteConfirm">Confirm</v-btn>
+                    <v-spacer />
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-btn
+                small
+                color="teal accent-3"
+                class="black--text"
+                @click="manageHotspot(item)"
+              >
+                Manage
+              </v-btn>
+              <v-btn
+                small
+                color="teal accent-3"
+                class="black--text"
+                @click="deleteHotspot(item)"
+              >
+                Remove
+              </v-btn>
+            </template>
+          </v-data-table>
         </v-card>
       </template>
     </ConsistentMP>
@@ -330,12 +511,16 @@ export default {
       UMdialog: false,
       VCdialog: false,
       VMdialog: false,
+      DelDialog: false,
+      HAdialog: false,
+      HMdialog: false,
       pop: false,
       popColor: '',
       popText: '',
       max255chars: v => v.length <= 255 || 'Input too long!',
-      userIndex: 0,
-      venueIndex: 0,
+      userIndex: -1,
+      venueIndex: -1,
+      hotspotIndex: -1,
       selectedUserID: '',
       selectedVenueID: '',
       usersSearch: '',
@@ -418,8 +603,9 @@ export default {
           value: 'affectedUsers'
         },
         {
-          text: 'Last created timestamp',
-          value: 'lastCreated'
+          text: '',
+          value: 'actions',
+          sortable: false
         }
       ],
       currentHotspotsItems: [
@@ -429,8 +615,7 @@ export default {
           latitude: -77.8419,
           longitude: 116.6863,
           radius: 1000,
-          affectedUsers: 30,
-          lastCreated: 1621147075
+          affectedUsers: 30
         },
         {
           venueID: 'abyz0201',
@@ -438,8 +623,7 @@ export default {
           latitude: -77.8477,
           longitude: 166.7602,
           radius: 800,
-          affectedUsers: 22,
-          lastCreated: 1621147077
+          affectedUsers: 22
         }
       ],
 
@@ -652,7 +836,18 @@ export default {
           value: 'No',
           updated: '2021-05-16'
         }
-      ]
+      ],
+      addHotspotDetails: {
+        venueID: '',
+        startTime: '',
+        endTime: '',
+        affectedUsers: ''
+      },
+      manageHotspotDetails: {
+        startTime: '2021/01/23 04:11:05',
+        endTime: '2021/01/23 04:11:05',
+        affectedUsers: 30
+      }
     }
   },
 
@@ -687,6 +882,22 @@ export default {
       this.VMdialog = true
     },
 
+    deleteHotspot(item) {
+      this.hotspotIndex = this.currentHotspotsItems.indexOf(item)
+      this.DelDialog = true
+    },
+
+    deleteConfirm() {
+      this.currentHotspotsItems.splice(this.hotspotIndex, 1)
+      this.close()
+    },
+
+    manageHotspot(item) {
+      this.hotspotIndex = this.currentHotspotsItems.indexOf(item)
+      this.selectedVenueID = this.currentHotspotsItems[this.hotspotIndex].venueID
+      this.HMdialog = true
+    },
+
     usave() {
       this.pop = true
       this.popColor = 'success'
@@ -715,6 +926,11 @@ export default {
       this.VMdialog = true
     },
 
+    hsave() {
+      this.HMdialog = false
+      this.HAdialog = false
+    },
+
     open() {
     },
 
@@ -723,6 +939,17 @@ export default {
       this.UMdialog = false
       this.VCdialog = false
       this.VMdialog = false
+      this.DelDialog = false
+      this.HAdialog = false
+      this.HMdialog = false
+    },
+
+    addHotspot() {
+      this.HAdialog = true
+    },
+
+    submitHotspot() {
+      this.HAdialog = false
     }
 
     // showUsers() {
