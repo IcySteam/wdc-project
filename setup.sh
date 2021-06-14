6062fbc48d3eafc9c608282bb90d519eb6b9d0d0
IcySteam <IcySteam@users.noreply.github.com>
2021-06-14 23:28:58 +0930

    cs50 compat

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # set up and run mysql first
-#cd docs
+#cd ./docs
 #mysql --host=localhost < wdc-project_database_schema.sql
 #mysql --host=localhost < mysql_test_data.sql
+chmod +x ./frontend/src/plugins/setup.sh
+.frontend/src/plugins/setup.sh
 cd ./backend
 npm install
 cd ../frontend
-chmod +x ./src/plugins/setup.sh
-./src/plugins/setup.sh
 npm install
